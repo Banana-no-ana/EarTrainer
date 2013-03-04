c8f314d42be4b39b98798a386391b082b5d7dcee
Daniel Piao <dimshadow101@gmail.com>
2013-03-03 17:29:47 -0800

    Adding nathan's images basic mainmenu change

---
+++ App/scripts/mainMenu.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$('#quickPlay').hover(function() {
+  $(this).attr('src', 'img/mainMenu/QuickplayHighlight.png');
+}, function() {
+  $(this).attr('src', 'img/mainMenu/Quickplay.png');
+});
+
+$('#advancedPlay').hover(function() {
+  $(this).attr('src', 'img/mainMenu/AdvancedplayHighlight.png');
+}, function() {
+  $(this).attr('src', 'img/mainMenu/Quickplay2.png');
+});
+
+$('#advancedPlay').hover(function() {
+  $(this).attr('src', 'img/mainMenu/Quickplay2_over.png');
+}, function() {
+  $(this).attr('src', 'img/mainMenu/Quickplay2.png');
+});
+
+$('#advancedPlay').hover(function() {
+  $(this).attr('src', 'img/mainMenu/Quickplay2_over.png');
+}, function() {
+  $(this).attr('src', 'img/mainMenu/Quickplay2.png');
+});
+
+$('#advancedPlay').hover(function() {
+  $(this).attr('src', 'img/mainMenu/Quickplay2_over.png');
+}, function() {
+  $(this).attr('src', 'img/mainMenu/Quickplay2.png');
+});
